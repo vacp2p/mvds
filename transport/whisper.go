b99f2564f4a77b55591da6afe72b37790b95314f
decanus <dean.eigenmann@icloud.com>
2019-04-20 17:28:04 +0200

    whisper node

---
+++ transport/whisper.go
@@ -0,0 +1,18 @@
+package transport
+
+type WhisperNode struct {
+
+}
+
+func NewWhisperNode() *WhisperNode {
+	return nil
+}
+
+
+func (n *WhisperNode) Tick() {
+	panic("implement me")
+}
+
+func (n *WhisperNode) SendMessage(senderId []byte, to []byte, message []byte) {
+	panic("implement me")
+}
