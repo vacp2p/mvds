df671e1eaf60d479b5eab3d8432a5052d1ec1fcf
decanus <dean.eigenmann@icloud.com>
2019-04-27 15:35:02 +0200

    todo

--- transport/node.go
+++ transport/node.go
@@ -1,6 +1,6 @@
 package transport
 
 type Node interface {
-	Watch() error
+	Watch() error // @todo this should return error or payload, watch should always watch for one message and then be called again
 	SendMessage(senderId []byte, to []byte, message []byte) error // @todo probably needs types
 }
