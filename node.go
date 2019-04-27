20a3510706c31892a1957b53dfa92596b4b2f210
decanus <dean.eigenmann@icloud.com>
2019-04-27 15:12:16 +0200

    wrapper func

--- node.go
+++ node.go
@@ -4,3 +4,10 @@ type Node struct {
 	Send     <-chan []byte
 	Received chan<- []byte // @todo message type
 }
+
+func (n *Node) Start() error {
+
+	// @todo start listening to both the send channel and what the transport receives for later handling
+
+	return nil
+}
