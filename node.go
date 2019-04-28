b52a50656d97045ce497b10af881d95fd0f806a3
decanus <dean.eigenmann@icloud.com>
2019-04-28 05:22:46 +0200

    added dummy wrapper

--- node.go
+++ node.go
@@ -1,6 +1,10 @@
 package mvds
 
+import "github.com/status-im/mvds/storage"
+
 type Node struct {
+	ms storage.MessageStore
+
 	Send     <-chan []byte
 	Received chan<- []byte // @todo message type
 }
@@ -10,4 +14,23 @@ func (n *Node) Start() error {
 	// @todo start listening to both the send channel and what the transport receives for later handling
 
 	return nil
+}
+
+func (n *Node) onRequest(message Request) {
+	for _, id := range message.Messages {
+		m, err := n.ms.GetMessage(id)
+		if err != nil {
+			// @todo
+		}
+
+		// @todo put message on the wire
+	}
+}
+
+func (n *Node) onAck() {
+
+}
+
+func (n *Node) onMessage() {
+
 }
