8823ef62cf7c81c0f7d82f3b2a61ae3993f48c5c
decanus <dean.eigenmann@icloud.com>
2019-05-01 00:40:23 +0200

    changed

--- securetransport/node.go
+++ securetransport/node.go
@@ -1,7 +1,5 @@
 package securetransport
 
-import "github.com/status-im/mvds"
-
 type Node interface {
-	SendPayload(senderId mvds.PeerId, to mvds.PeerId, payload mvds.Payload) error
+	Send(data []byte) error
 }
