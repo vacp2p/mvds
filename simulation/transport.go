9201d4958c0bcd54b43fd39992be265e39c22e73
decanus <dean.eigenmann@icloud.com>
2019-05-03 18:18:00 +0200

    started working on v dummy transport

--- simulation/transport.go
+++ simulation/transport.go
@@ -6,8 +6,19 @@ import (
 	"github.com/status-im/mvds"
 )
 
+type packet struct {
+	sender  mvds.PeerId
+	payload mvds.Payload
+}
+
 type Transport struct {
-	out map[mvds.PeerId]chan mvds.Payload
+	in <- chan packet
+	out map[mvds.PeerId]chan <-packet
+}
+
+func (t *Transport) Watch() (mvds.PeerId, mvds.Payload) {
+	p := <- t.in
+	return p.sender, p.payload
 }
 
 func (t *Transport) Send(sender mvds.PeerId, peer mvds.PeerId, payload mvds.Payload) error {
@@ -16,7 +27,6 @@ func (t *Transport) Send(sender mvds.PeerId, peer mvds.PeerId, payload mvds.Payl
 		return errors.New("peer unknown")
 	}
 
-	c <- payload
+	c <- packet{sender: sender, payload: payload}
 	return nil
 }
-
