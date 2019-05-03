a0e08a6aeb7ff79a30a92b2117d08917e42aa373
decanus <dean.eigenmann@icloud.com>
2019-05-03 22:30:52 +0200

    started init for simulation

--- simulation/simulation.go
+++ simulation/simulation.go
@@ -1,5 +1,14 @@
 package main
 
+import "github.com/status-im/mvds"
+
 func main() {
 
+	ain := make(chan packet)
+	bin := make(chan packet)
+	cin := make(chan packet)
+
+	at := Transport{in: ain, out: make(map[mvds.PeerId]chan<- packet)}
+	bt := Transport{in: bin, out: make(map[mvds.PeerId]chan<- packet)}
+	ct := Transport{in: cin, out: make(map[mvds.PeerId]chan<- packet)}
 }
