2b9467217abb513abb5aa60427ab33667cd89351
decanus <dean.eigenmann@icloud.com>
2019-05-27 10:23:08 -0400

    changes function fully to map

--- state.go
+++ state.go
@@ -46,16 +46,11 @@ func (s syncState) Iterate() map[GroupID]map[MessageID]map[PeerId]state {
 	return s.state
 }
 
-func (s syncState) Map(callback func(g GroupID, m MessageID, p PeerId, s state) *state) {
+func (s syncState) Map(process func(g GroupID, m MessageID, p PeerId, s state) state) {
 	for group, syncstate := range s.state {
 		for id, peers := range syncstate {
 			for peer, state := range peers {
-				newState := callback(group, id, peer, state)
-				if newState == nil {
-					continue
-				}
-
-				s.state[group][id][peer] = *newState
+				s.state[group][id][peer] = process(group, id, peer, state)
 			}
 		}
 	}
