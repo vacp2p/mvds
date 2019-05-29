d04eae22cf8502c80f16fbfa7b4219334239a016
decanus <dean.eigenmann@icloud.com>
2019-05-28 20:44:40 -0400

    minor cleanup

--- state.go
+++ state.go
@@ -1,63 +1,13 @@
 package mvds
 
-import "sync"
-
 type state struct {
 	SendCount   uint64
 	SendEpoch   int64
 }
 
-type syncState struct {
-	sync.Mutex
-
-	state map[GroupID]map[MessageID]map[PeerId]state
-}
-
-func newSyncState() syncState {
-	return syncState{
-		state: make(map[GroupID]map[MessageID]map[PeerId]state),
-	}
-}
-
-func (s syncState) Get(group GroupID, id MessageID, sender PeerId) state {
-	s.Lock()
-	defer s.Unlock()
-
-	state, _ := s.state[group][id][sender]
-	return state
-}
-
-func (s *syncState) Set(group GroupID, id MessageID, sender PeerId, newState state) {
-	s.Lock()
-	defer s.Unlock()
-
-	if _, ok := s.state[group]; !ok {
-		s.state[group] = make(map[MessageID]map[PeerId]state)
-	}
-
-	if _, ok := s.state[group][id]; !ok {
-		s.state[group][id] = make(map[PeerId]state)
-	}
-
-	s.state[group][id][sender] = newState
-}
-
-func (s *syncState) Remove(group GroupID, id MessageID, sender PeerId) {
-	s.Lock()
-	defer s.Unlock()
-
-	delete(s.state[group][id], sender)
-}
-
-func (s *syncState) Map(process func(g GroupID, m MessageID, p PeerId, s state) state) {
-	s.Lock()
-	defer s.Unlock()
-
-	for group, syncstate := range s.state {
-		for id, peers := range syncstate {
-			for peer, state := range peers {
-				s.state[group][id][peer] = process(group, id, peer, state)
-			}
-		}
-	}
+type syncState interface {
+	Get(group GroupID, id MessageID, sender PeerId) state
+	Set(group GroupID, id MessageID, sender PeerId, newState state)
+	Remove(group GroupID, id MessageID, sender PeerId)
+	Map(process func(g GroupID, m MessageID, p PeerId, s state) state)
 }
